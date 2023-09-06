6d71972348e1e996d13e8a3e83d6d73734a837e1
Ricardo Ribeiro <richy.jsr@hotmail.com>
2023-09-06 20:52:51 +0100

    Animation Parallax

--- components/AnimationComponent.tsx
+++ components/AnimationComponent.tsx
@@ -8,19 +8,28 @@ export default function AnimationComponent (props:any) {
 
     return(
         <div>
-            <div className="relative flex justify-center items-center w-[100%]">
-                <img className="w-[100%] z-40" src="/animation/Ring.png" alt=""></img>
+            <MouseParallaxContainer resetOnLeave containerStyle={{overflow:"visible"}} globalFactorX={0.1} globalFactorY={0.1} className="relative flex justify-center items-center w-[100%]">
+                <MouseParallaxChild factorY={0.5} factorX={0} className="z-40"><img className="w-[100%] " src="/animation/Ring.png" alt="Ring"></img></MouseParallaxChild>
                 <div className=" flex justify-center w-[100%] absolute ">
-                    <img className=" w-[100%]"  src={`animation/${props.title}/Background.png`} alt=""></img>
+                    <img className=" w-[100%]" src={`animation/${props.title}/Background.png`} alt="Background"></img>
                 </div>
+                <MouseParallaxChild factorX={0.3} factorY={0.3} className="flex justify-center left-0 right-0 m-auto absolute z-10">
+                    <img className="w-[100%] -translate-y-[20%]" src={`animation/${props.title}/Char.png`} alt="Character"></img> 
+                </MouseParallaxChild>
+                <div className="flex justify-center w-[100%] absolute z-10">
+                    <img className="w-[100%]" src="/animation/BlackGradient.png" alt="Gradient"></img>
+                </div>
+                
 
-                <div className="flex justify-center -translate-y-[22%] left-0 right-0 m-auto absolute z-10">
-                    <img className="w-[100%]" src={`animation/${props.title}/Char.png`} alt=""></img> 
-                </div>
                 <div className=" flex justify-center w-[100%] absolute">
-                    <img className="w-[100%]" src="/animation/Border.png" alt=""></img>
+                    <img className="w-[100%]" src="/animation/Border.png" alt="Border"></img>
                 </div>
-            </div>
+                <div className="absolute bottom-[35px] left-0 right-0 m-auto z-10">
+                    <h2>{props.title}</h2>
+                    <h3 className="mb-[10px]">{props.categorie}</h3>
+                    <Button title={"Take Me There"} width="Big"></Button> 
+                </div>
+            </MouseParallaxContainer>
         </div>
     )
 }
--- components/AnimationSection.tsx
+++ components/AnimationSection.tsx
@@ -2,53 +2,30 @@ import AnimationComponent from "./AnimationComponent";
 
 export default function AnimationSection (){
     return(
-        <div className="flex flex-col w-screen justify-center items-center overflow-x-hidden">
-            <div> 
-                <AnimationComponent title={"Famous Foxes"} categorie={"Marketing Trailer"}></AnimationComponent>
-            </div> 
-            <div> 
-                <AnimationComponent title={"Elixir"} categorie={"Marketing Trailer"}></AnimationComponent>
-            </div> 
-            <div> 
-                <AnimationComponent title={"Sandbar"} categorie={"Marketing Trailer"}></AnimationComponent>
-            </div> 
-            <div> 
-                <AnimationComponent title={"Toys R Us"} categorie={"Marketing Trailer"}></AnimationComponent>
-            </div> 
-            <div> 
-                <AnimationComponent title={"Reign Of Titans"} categorie={"Marketing Trailer"}></AnimationComponent>
-            </div> 
-            <div> 
-                <AnimationComponent title={"Nekkro"} categorie={"Marketing Trailer"}></AnimationComponent>
-            </div> 
-            <div> 
-                <AnimationComponent title={"Clear Collectibles"} categorie={"Marketing Trailer"}></AnimationComponent>
-            </div> 
-            <div> 
-                <AnimationComponent title={"Tombstoned"} categorie={"Marketing Trailer"}></AnimationComponent>
-            </div> 
-            <div> 
-                <AnimationComponent title={"Midas"} categorie={"Marketing Trailer"}></AnimationComponent>
-            </div> 
-            <div> 
-                <AnimationComponent title={"Doordash"} categorie={"Marketing Trailer"}></AnimationComponent>
-            </div> 
-            <div> 
-                <AnimationComponent title={"ABC"} categorie={"Marketing Trailer"}></AnimationComponent>
-            </div> 
-            <div> 
-                <AnimationComponent title={"Rugpass"} categorie={"Marketing Trailer"}></AnimationComponent>
-            </div> 
-            <div> 
-                <AnimationComponent title={"021"} categorie={"Marketing Trailer"}></AnimationComponent>
-            </div> 
-            <div> 
-                <AnimationComponent title={"Oracles"} categorie={"Marketing Trailer"}></AnimationComponent>
-            </div> 
-            <div> 
-                <AnimationComponent title={"Just Ape"} categorie={"Marketing Trailer"}></AnimationComponent>
-            </div> 
-           
+        <div className="scroll-container">
+            <section className="grid grid-cols-3 w-screen justify-center items-center">
+                <div> 
+                    <AnimationComponent title={"Famous Foxes"} categorie={"Marketing Trailer"}></AnimationComponent>
+                </div> 
+                <div> 
+                    <AnimationComponent title={"Elixir"} categorie={"Marketing Trailer"}></AnimationComponent>
+                </div> 
+                <div> 
+                    <AnimationComponent title={"Sandbar"} categorie={"Marketing Trailer"}></AnimationComponent>
+                </div> 
+            </section>
+
+            <section className="grid grid-cols-3 w-screen h-screen justify-center items-center">
+                <div> 
+                    <AnimationComponent title={"Famous Foxes"} categorie={"Marketing Trailer"}></AnimationComponent>
+                </div> 
+                <div> 
+                    <AnimationComponent title={"Elixir"} categorie={"Marketing Trailer"}></AnimationComponent>
+                </div> 
+                <div> 
+                    <AnimationComponent title={"Sandbar"} categorie={"Marketing Trailer"}></AnimationComponent>
+                </div> 
+            </section>
         </div>
     )
 }
